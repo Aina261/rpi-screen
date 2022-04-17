b12808bebe47e5571929ccc0788dce2b95ac3e0c
Aina <36484918+Aina261@users.noreply.github.com>
2022-04-17 09:19:35 +0200

    Update readme.md

--- README.sh
+++ README.sh
@@ -1,3 +1,8 @@
 # Installation
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Aina261/rpi-screen/main/install.sh)"
+You need to run it with sudo.
+```sh
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/Aina261/rpi-screen/main/install.sh)"
+```
+
+Functionnal with raspberry pi 4b 2 / 4 / 8 gb on Ubuntu server
